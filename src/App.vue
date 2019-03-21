4e6e423d5a8d5ece87c6f03c6ab26028b921fc15
QuinJetZhou <zwq0807@gmail.com>
2019-03-21 14:51:35 +0800

    添加一点点

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul class="layui-nav" lay-filter="">
+        <li class="layui-nav-item"><router-link to="/">Home</router-link></li>
+        <li class="layui-nav-item layui-this"><router-link to="/about">About</router-link></li>
+        <li class="layui-nav-item"><a href="">社区</a></li>
+      </ul>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+
+      
+}
+
+
+</script>
 
 <style>
+@import '../node_modules/layui-src/build/css/layui.css';
+body{
+  background: #F4F6F6;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,7 +37,7 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
 }
 
 #nav a.router-link-exact-active {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import jQuery from '../node_modules/jquery/dist/jquery'
+// import layui from '../node_modules/layui-src/build/layui'
+// import element from '../node_modules/layui-src/build/lay/modules/element'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
